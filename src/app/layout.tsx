f94764858341ea0c4b4eae0eedc38d015972de37
quantum-builds <muhammadmassab.dev@gmail.com>
2025-04-25 14:31:58 +0500

    add:icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import "./globals.css";
 import { TanStackProvider } from "@/providers/TanStackProvider";
 import { ToasterProvider } from "@/providers/ToasterProvider";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Aspire Academy",
+  icons: {
+    icon: "/aspire-consent-white-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className="bg-white">
