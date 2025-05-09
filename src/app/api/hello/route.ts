cf7bc341156d029cea4cac512d1367cfff6a240f
quantum-builds <muhammadmassab.dev@gmail.com>
2025-05-09 18:28:45 +0500

    testing: cron-job

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,8 @@
+import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const mcq = await prisma.mCQ.findMany({});
+  console.log(mcq);
   return NextResponse.json("Hello from Vercel!");
 }
