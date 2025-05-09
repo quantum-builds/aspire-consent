93c5fb07baef05416b5eac6b5f6354145eaf8c71
quantum-builds <muhammadmassab.dev@gmail.com>
2025-05-09 18:33:22 +0500

    testing: cron-job

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,7 +2,30 @@ import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const mcq = await prisma.mCQ.findMany({});
-  console.log(mcq);
-  return NextResponse.json("Hello from Vercel!");
+  // const mcq = await prisma.mCQ.findMany({});
+  // console.log(mcq);
+  // return NextResponse.json("Hello from Vercel!");
+  const now = new Date();
+
+  try {
+    const updated = await prisma.consentFormLink.updateMany({
+      where: {
+        expiresAt: { lte: now },
+        status: {
+          in: ["COMPLETED", "IN_PROGRESS", "PENDING"],
+        },
+      },
+      data: {
+        status: "EXPIRED",
+      },
+    });
+
+    return NextResponse.json({ updated: updated.count });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
